895698d0f12600bacb8b5196db5a1d70d44eee08
Mariolandaf <mariolandaf@hotmail.com>
2021-08-25 22:31:02 -0500

    Add files via upload

---
+++ app/js/sweet.js
@@ -0,0 +1,24 @@
+function desarrollo() {
+    Swal.fire ({
+    title: "Website under development...",
+    icon: "info",
+    toast: true,
+    timer: 4000,
+    position: "top",
+    confirmButtonColor: "#004aad",
+    confirmButtonArialLabel: "Página en desarrollo",
+    allowOutsideClick: true,
+})
+}
+
+function contacto() {
+    Swal.fire ({
+    title: "Registro Exitoso",
+    text: "Avísperas del futuro",
+    icon: "success",
+    toast: true,
+    position: "center",
+    confirmButtonColor: "#004aad",
+    confirmButtonArialLabel: "Registro Exitoso",
+})
+}
